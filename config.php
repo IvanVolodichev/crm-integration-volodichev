b6b32ca2103d90c6c51c871e98d5de609a11bda6
Ivan Volodichev <ivan_vol@icloud.com>
2025-07-15 15:54:11 +0300

    исправленный конфиг

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ return [
     'amoCRM' => [
         'client_id' => 'a0faa7c2-3672-431e-aa0e-fc5e4f6d7acf',
         'client_secret' => 'rg8eZvghpWqhQF1gzY3SkpYC7yrS2RGz7Wlp9X92Hjd5As2zrOLJ6PZTHr2OFM6n',
-        // 'redirectUri' => 'http://cognitive.beget.tech/amo_auth.php', //для хоста
-        'redirect_uri' => 'http://localhost:8000/amo_auth.php', // для локалки
+        'redirectUri' => 'http://cognitive.beget.tech/amo_auth.php', //для хоста
+        // 'redirect_uri' => 'http://localhost:8000/amo_auth.php', // для локалки
         'token_file' => __DIR__ . '/tokens.json',
     ],
 
